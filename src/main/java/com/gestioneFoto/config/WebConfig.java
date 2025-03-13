e459680e995b0871ebc026b0b0ad53189f1dead3
Costantino-Scardigno <costantino98@icloud.com>
2025-03-13 18:05:25 +0100

    first commit

--- src/main/java/com/gestioneFoto/config/WebConfig.java
+++ src/main/java/com/gestioneFoto/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://scattifestosi.netlify.app/") // oppure "*" per permettere tutte le origini (non consigliato in produzione)
+                .allowedOrigins("http://scattifestosi-back-end-production.up.railway.app/") // oppure "*" per permettere tutte le origini (non consigliato in produzione)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
